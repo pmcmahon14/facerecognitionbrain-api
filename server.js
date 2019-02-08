209dc77e8e3f4f441c145ce0c7d6bd3f9ae99fc8
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-08 13:50:01 -0800

    installed knex and pg for database connection

--- server.js
+++ server.js
@@ -1,55 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 //need bodyParser for express to properly read users objects
-app.use(bodyParser.json());
+
 const database = {
     users: [
         {
             id: '123',
             name: 'John',
+            password: 'cookies',
             email: 'john@gmail.com',
-            //password: 'cookies',
             entries: 0,
             joined: new Date()
         },
         {
             id: '124',
             name: 'Sally',
+            password: 'bananas',
             email: 'sally@gmail.com',
-            //password: 'bananas',
             entries: 0,
             joined: new Date()
         }
     ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: "john@gmail.com"
-        }
-    ]
 };
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-    // Load hash from your password DB.
-    bcrypt.compare("apples", '$2a$10$JgA889TsjfHFEsyn9aJCluc0IFJgXi/ZVxHM6ziUIEzjBadGct7FG'
-        , function(err, res) {
-        console.log('first guess', res);
-    });
-    bcrypt.compare("veggies", '$2a$10$JgA889TsjfHFEsyn9aJCluc0IFJgXi/ZVxHM6ziUIEzjBadGct7FG', function(err, res) {
-        console.log('second guess', res);
-    });
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-        res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in');
     }
@@ -57,15 +46,15 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    /*bcrypt.hash(password, null, null, function(err, hash) {
+    //do i still need bcrypt below
+    bcrypt.hash(password, null, null, function(err, hash) {
         console.log(hash);
-    });*/
+    });
 
     database.users.push({
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     });
@@ -86,7 +75,7 @@ app.get('/profile/:id', (req, res) => {
     }
 });
 
-app.put('/image', (eq, res) => {
+app.put('/image', (req, res) => {
     const {id} = req.body;
     let found = false;
     database.users.forEach(user => {
