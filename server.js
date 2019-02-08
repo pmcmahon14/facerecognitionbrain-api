2e49a58e74985fa9a8f1bc0d2d0e3c9558c9293d
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-08 13:55:07 -0800

    added knex const to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
 const app = express();
 
