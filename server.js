a576445fa5346505ee941eacf71f8720f5979f0d
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-14 16:49:19 -0800

    added environmental variable for port on server.js

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)});
 app.put('/image', (req,res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 });
 
-
+console.log(PORT);
